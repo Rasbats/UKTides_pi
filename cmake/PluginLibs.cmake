6822bc9155af3463a2741256bc6e09ba739e61b5
Rasbats <miker@members.swis.net>
2021-02-14 12:37:38 +0000

    Update PluginLibs.cmake
    Add set(BUILD_SHARED_LIBS TRUE)

--- cmake/PluginLibs.cmake
+++ cmake/PluginLibs.cmake
@@ -32,13 +32,11 @@ if (APPLE)
 endif ()
 
 set(wxWidgets_USE_LIBS base core net xml html adv stc)
+set(BUILD_SHARED_LIBS TRUE)
 
 find_package(wxWidgets REQUIRED base core net xml html adv stc)
 if (MSYS)
   # This is just a hack. I think the bug is in FindwxWidgets.cmake
-  string(
-    REGEX REPLACE "/usr/local" "\\\\;C:/MinGW/msys/1.0/usr/local"
-    wxWidgets_INCLUDE_DIRS ${wxWidgets_INCLUDE_DIRS}
-  )
+  string(REGEX REPLACE "/usr/local" "\\\\;C:/MinGW/msys/1.0/usr/local" wxWidgets_INCLUDE_DIRS ${wxWidgets_INCLUDE_DIRS})
 endif ()
 include(${wxWidgets_USE_FILE})
